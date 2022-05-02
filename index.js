b9cd46f31bd35be3a732cbee3802b2908008d673
Calvo <julio.calvo@itu.int>
2022-05-02 13:13:44 +0200

    Not tested

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 'use strict';
 const needle = require('needle');
 
@@ -11,6 +12,44 @@ const needle = require('needle');
 // 3. Finally write a function named "awaitUppercaseQuote"
 //    that wraps "promiseQuote" using async/await and
 //    returns the content of the quote object in uppercase.
+
+function getQuote (someUrl) {
+  needle.get(someUrl, {method: 'Quote'}, function (error, response) {
+    if (!error && response.statusCode == 200) {
+      return console.log(response.body);
+    }
+    else return Eventer.addStatus( {type:'error', source:'url', id:rule.name, text:response.statusMessage });
+  });
+}
+
+async function Callee () {
+  console.log('calling');
+  const result = getQuote('http://www.google.com');
+  console.log(result);
+}
+
+function promiseQuote (someUrl) {
+  return new Promise(resolve => {
+    needle.get(someUrl, {method: 'Quote'}, function (error, response) {
+      if (!error && response.statusCode == 200) {
+        return console.log(response.body);
+      }
+      else return Eventer.addStatus( {type:'error', source:'url', id:rule.name, text:response.statusMessage });
+    });
+  });
+}
+
+async function awaitUppercaseQuote (someUrl) {
+  console.log('calling');
+  const result = await promiseQuote(someUrl);
+  console.log(result);
+}
+
+awaitUppercaseQuote('http://www.google.com');
+
+
+
+Callee();
 
 module.exports = {
   getQuote,
